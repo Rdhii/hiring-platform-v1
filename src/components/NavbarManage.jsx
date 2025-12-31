27816a191f5d0fff67263cdbfeb9b00e5ac3dd02
Rdhii <radheibelajar@gmail.com>
2025-12-31 14:41:25 +0700

    add link to go back to joblist page in manage  candidate page

--- src/components/NavbarManage.jsx
+++ src/components/NavbarManage.jsx
@@ -1,11 +1,12 @@
 import { ArrowRight } from "lucide-react";
 import profileIcon from "../assets/vecteezy_profile-icon-design-vector_5544718.jpg";
+import { Link } from "react-router-dom";
 
 export default function NavbarManage() {
   return (
     <nav className="flex items-center justify-between px-5 py-4  border-b border-gray-200">
         <div className="flex items-center font-medium text-sm gap-2">
-            <p className=" py-1 px-4 rounded-lg border border-gray-300 shadow-sm">Job List</p>
+            <Link to="/" className=" py-1 px-4 rounded-lg border border-gray-300 shadow-sm">Job List</Link>
             <ArrowRight />
             <p className="py-1 px-4 rounded-lg border border-gray-300  bg-[#EDEDED]"    >Manage Candidate</p>
         </div>
