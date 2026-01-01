991b9536ea06c083ccd2d01e6d7dbeb9f7cb18a9
Rdhii <radheibelajar@gmail.com>
2026-01-01 21:45:15 +0700

    .

--- src/components/NavbarManage.jsx
+++ src/components/NavbarManage.jsx
@@ -8,7 +8,7 @@ export default function NavbarManage() {
         <div className="flex items-center font-medium text-sm gap-2">
             <Link to="/" className=" py-1 px-4 rounded-lg border border-gray-300 shadow-sm">Job List</Link>
             <ArrowRight />
-            <p className="py-1 px-4 rounded-lg border border-gray-300  bg-[#EDEDED]"    >Manage Candidate</p>
+            <p className="py-1 px-4 rounded-lg border border-gray-300 bg-[#EDEDED]">Manage Candidate</p>
         </div>
         <img src={profileIcon} alt="user avatar" className="size-7 border border-gray-300 rounded-full" />
     </nav>
