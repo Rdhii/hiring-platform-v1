402590de27de0bf310313d7f877a99135c746bdb
Rdhii <radheibelajar@gmail.com>
2026-01-31 20:37:28 +0700

    .

--- src/components/admin/Navbar.jsx
+++ src/components/admin/Navbar.jsx
@@ -2,7 +2,7 @@ import profileIcon from "../../assets/vecteezy_profile-icon-design-vector_554471
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-5 py-2 border-b border-gray-200    ">
+    <nav className="flex items-center justify-between px-5 py-2 border-b border-gray-200">
         <p className=" py-2.5 font-medium text-lg">Job List</p>
         <img src={profileIcon} alt="user avatar" className="size-7 border border-gray-300 rounded-full" />
     </nav>
